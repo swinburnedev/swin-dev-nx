d662bb441d0ea9060a746454bfcf6c5f9d7ec883
Andy Swinburne <andy@swinburne.dev>
2022-11-21 21:05:30 +0000

    styling

--- apps/swinburne-dev/pages/index.tsx
+++ apps/swinburne-dev/pages/index.tsx
@@ -8,17 +8,17 @@ export function Index() {
         <Layout>
           <div className="grid">
             <div className="grid-rows-1 grid-cols-1">
-                <h1 className="text-3xl sm:text-4xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-10">
+                <h1 className="text-3xl sm:text-4xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 pt-10">
                     Hi, I'm Andy &#128075;
                 </h1>
             </div>
             <div className="grid grid-cols-2 gap-4">
                   <div className="max-w-4xl">
-                  <h2>Senior Frontend Engineer</h2>
+                  <h2 className="text-xl sm:text-xl font-bold text-slate-900 tracking-tight dark:text-slate-200 pt-5">Senior Frontend Engineer</h2>
 
-                    <p>A little bit more about me:</p>
+                    <p className="py-4">A little bit more about me:</p>
                     <i>Personal:</i>
-                    <ul>
+                    <ul className="py-4">
                       <li>Married</li>
                       <li>2 kids</li>
                       <li>Runner</li>
@@ -26,19 +26,21 @@ export function Index() {
                       <li>Coder</li>
                     </ul>
                     <i>Professional:</i>
-                    <ul>
-                      <li>15 years experience</li>
+                    <ul className="py-4">
+                      <li>15+ years experience</li>
                       <li>Frontend engineer</li>
                       <li>React</li>
                       <li>Performance</li>
                       <li>Accessibility</li>
                       <li>Usability</li>
-                      <li>Full CV</li>
                     </ul>
-                    <Link className="block pt-10" href='/projects'>
+                    <ul className="py-4">
+                      <li><Link href='/cv'>View full CV</Link></li>
+                      <li><Link href='/projects'>
                         Projects
-                    </Link>
-                    <p>Code for this site, take a look:</p>
+                    </Link></li>
+                      <li><Link href='https://github.com/swinburnedev/swin-dev-nx'>Code for this site, take a look</Link></li>
+                    </ul>
                   </div>
                   <div className="max-w-4xl">
                     <ImageCaption 
