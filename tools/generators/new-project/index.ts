705a1bc405026b5721727dad9c5f8bfbcf876817
Andy Swinburne <andy@swinburne.dev>
2022-11-21 21:05:24 +0000

    Add project mdx generator

---
+++ tools/generators/new-project/index.ts
@@ -0,0 +1,20 @@
+import { Tree, formatFiles, generateFiles, joinPathFragments, names } from '@nrwl/devkit';
+
+interface ProjectSchemaOptions {
+  title: string;
+  excerpt?: string
+}
+export default async function (tree: Tree, schema: ProjectSchemaOptions) {
+  generateFiles(
+    tree,
+    joinPathFragments(__dirname, './files'),
+    './_projects',
+    {
+      title: schema.title,
+      excerpt: schema.excerpt || '',
+      normalizedTitle: names(schema.title).fileName,
+      creationDate: new Date().toISOString()
+    }
+  );
+  await formatFiles(tree);
+}
