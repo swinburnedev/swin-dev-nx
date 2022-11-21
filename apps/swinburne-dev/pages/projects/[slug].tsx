eacb668370e5faf328366779a62879eec7caf6c3
Andy Swinburne <andy@swinburne.dev>
2022-11-21 21:05:37 +0000

    add project tech tags and richtext

--- apps/swinburne-dev/pages/projects/[slug].tsx
+++ apps/swinburne-dev/pages/projects/[slug].tsx
@@ -3,27 +3,59 @@ import { gql } from '@apollo/client';
 import { ParsedUrlQuery } from 'querystring';
 import client from '../../apollo/client';
 import { Layout } from '../../components/layout';
-import Link from 'next/link';
-import { BackButton } from '@swin-dev-nx/shared/ui';
+import { BackButton, Chips, IChip, ContentfulRichText } from '@swin-dev-nx/shared/ui';
 
-/* eslint-disable-next-line */
-export interface ProjectProps extends ParsedUrlQuery {
-  title: string
+interface TagsCollection {
+  items: Array<IChip>
 }
 
-export function Project({brand, company, description, title, url}: ProjectProps) {
+interface Image {
+  description: string,
+  url: string,
+}
+
+export interface ProjectProps extends ParsedUrlQuery {
+  brand: string,
+  company: string,
+  description?: {
+    json: any
+  },
+  screenshotDesktop: Image,
+  title: string,
+  tagsCollection: TagsCollection,
+  url: string,
+}
+
+export function Project({brand, company, description, screenshotDesktop, title, tagsCollection, url}: ProjectProps) {
   return (
     <Layout>
         <article>
           <div className='flex align-middle pt-10'>
-            <h1 className='text-3xl sm:text-4xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200'>{title}</h1>
+            <h1 className='text-3xl sm:text-4xl font-extrabold text-slate-800 tracking-tight'>{title}</h1>
             <BackButton href='/projects' label='Back to Projects' className='ml-auto' />
           </div>
-          <h2 className="text-xl sm:text-xl font-bold text-slate-900 tracking-tight dark:text-slate-200 pt-5">{brand}</h2>
-          <hr />
-          <h3>{company}</h3>
-          {/* <div className='prose prose-lg'>{description}</div> */}
-          <hr />
+          <div className='flex py-10'>
+            <div className='grow'>
+              <h2 className=" text-xl sm:text-xl font-bold text-slate-900 tracking-tight">{brand}</h2>
+              {company && <span className="text-sm text-slate-600">{company}</span>}
+            </div>
+            <Chips chips={tagsCollection.items} />
+          </div>
+          <hr className="pb-10"/>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="mb-8">
+              <ContentfulRichText document={description?.json} />
+            </div>
+            <div className="mb-4">
+              { screenshotDesktop &&
+                <img
+                  src={screenshotDesktop.url}
+                  className="max-w-full h-auto rounded-lg"
+                  alt={screenshotDesktop.description}
+                />
+              }
+            </div>
+          </div>
         </article>
     </Layout>
   );
@@ -42,8 +74,22 @@ export const getStaticProps: GetStaticProps = async ({
                   description {
                     json
                   }
+                  screenshotDesktop {
+                    description
+                    url
+                  }
                   slug
                   title
+                  tagsCollection(limit: 5) {
+                    items {
+                      id
+                      name
+                      icon {
+                        description
+                        url
+                      }
+                    }
+                  }
                   url
                 }
             }
