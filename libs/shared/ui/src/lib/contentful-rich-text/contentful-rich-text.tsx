154f54c8c46faab6513dd2025cddbbad7ec05bc8
Andy Swinburne <andy@swinburne.dev>
2022-11-26 14:38:47 +0000

    contentful richtext updates

--- libs/shared/ui/src/lib/contentful-rich-text/contentful-rich-text.tsx
+++ libs/shared/ui/src/lib/contentful-rich-text/contentful-rich-text.tsx
@@ -1,12 +1,11 @@
+import { ReactElement } from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { Document } from '@contentful/rich-text-types';
 
-export interface ContentfulRichTextProps {
+export interface IContentfulRichTextProps {
   document: Document
 }
 
-export function ContentfulRichText({document}: ContentfulRichTextProps) {
-  return documentToReactComponents(document);
+export const ContentfulRichText = ({document}: IContentfulRichTextProps): ReactElement => {
+  return (<div className='rich-text'>{ documentToReactComponents(document) }</div>)
 }
-
-export default ContentfulRichText;
