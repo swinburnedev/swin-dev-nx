7c08ab3591216ca0f741363b3ea5af5a760c104a
Andy Swinburne <andy@swinburne.dev>
2022-11-26 14:37:22 +0000

    add new logo

--- libs/shared/ui/src/lib/navbar/navbar.tsx
+++ libs/shared/ui/src/lib/navbar/navbar.tsx
@@ -10,8 +10,7 @@ export function Navbar() {
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
       <a href="/" className="flex items-center">
-          <img src="/svg/swindev_logo.svg" className="mr-3 h-6 sm:h-9" alt="Swindev Logo" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap text-slate-800">Swinburne.dev</span>
+          <img src="/images/swin-dev-square-128.png" className="mr-3 mt-2 h-16" alt="Swinburne Development Ltd" />
       </a>
       <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-language-select">
         <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
