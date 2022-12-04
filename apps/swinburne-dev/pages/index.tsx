148a1591f63e32cdb40c45d99ff31d7b2e21b350
Andy Swinburne <andy@swinburne.dev>
2022-12-04 19:56:06 +0000

    home copy changes

--- apps/swinburne-dev/pages/index.tsx
+++ apps/swinburne-dev/pages/index.tsx
@@ -11,7 +11,7 @@ export function Index() {
                 <h1 className="text-3xl sm:text-4xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 pt-10">
                     Hi, I'm Andy <span role="img" aria-label="waving hand" style={{lineHeight: 1}}>👋</span>
                 </h1>
-                <h2 className="text-xl sm:text-xl font-bold text-slate-900 tracking-tight dark:text-slate-200 pt-5">Senior Frontend Engineer</h2>
+                <h2 className="text-xl sm:text-xl font-bold text-slate-900 tracking-tight dark:text-slate-200 pt-5">Contract Senior Frontend Engineer</h2>
             </div>
             <div className="grid lg:grid-cols-2 md:grid-cols-1 gap-4">
                 <div className="max-w-4xl">
@@ -40,7 +40,9 @@ export function Index() {
                     </div>
                     <ul className="py-4">
                       <li><p>Want to see the code for this site? <Link href='https://github.com/swinburnedev/swin-dev-nx'>Take a look on Github</Link> <span role="img" aria-label="eyes" style={{lineHeight: 1}}>👀</span></p></li>
-                      <li className="block py-4">Or view my <Link href='/pdf/CV_Swinburne_Andy_website.pdf'>full CV</Link></li>
+                      <li className="block py-4">
+                        <p>Have a closer look at some key <Link href='/projects'>Projects</Link> or view my <Link href='/pdf/CV_Swinburne_Andy_website.pdf'>full CV</Link>.</p>
+                      </li>
                     </ul>
                   </div>
                   <div className="max-w-4xl">
