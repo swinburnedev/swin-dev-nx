9a46a6f102c222dd6ebb94d6b326a03cc53ea240
Andy Swinburne <andy.swinburne@purplebricks.com>
2022-12-07 10:58:03 +0000

    add contentful preview

--- apps/swinburne-dev/pages/projects/[slug].tsx
+++ apps/swinburne-dev/pages/projects/[slug].tsx
@@ -4,135 +4,169 @@ import { ParsedUrlQuery } from 'querystring';
 import client from '../../apollo/client';
 import { Layout } from '../../components/layout';
 import {
-  BackButton,
-  Chips,
-  IChip,
-  ContentfulRichText,
-  MobilePreview,
+    BackButton,
+    Chips,
+    IChip,
+    ContentfulRichText,
+    MobilePreview,
 } from '@swin-dev-nx/shared/ui';
 import { Document } from '@contentful/rich-text-types';
 
 interface TagsCollection {
-  items: Array<IChip>
+    items: Array<IChip>;
 }
 
 interface IImage {
-  description: string,
-  url: string,
+    description: string;
+    url: string;
 }
 
 interface IParams extends ParsedUrlQuery {
-  slug: string
+    slug: string;
 }
 
 export interface IProjectProps {
-  brand: string,
-  company: string,
-  description?: {
-    json: Document
-  },
-  screenshotMobile: IImage,
-  title: string,
-  tagsCollection: TagsCollection,
-  url?: string,
+    brand: string;
+    company: string;
+    description?: {
+        json: Document;
+    };
+    screenshotMobile: IImage;
+    title: string;
+    tagsCollection: TagsCollection;
+    url?: string;
 }
 
-export function Project({brand, company, description, screenshotMobile, title, tagsCollection}: IProjectProps) {
-  return (
-    <Layout>
-        <article>
-          <div className='md:flex align-middle pt-10'>
-            <h1 className='text-3xl sm:text-4xl font-extrabold text-slate-800 tracking-tight'>{title}</h1>
-            <BackButton href='/projects' label='Back to Projects' className='hidden md:flex ml-auto mt-4 md:mt-0' />
-          </div>
-          <div className='md:flex py-10'>
-            <div className='grow'>
-              <h2 className="text-xl sm:text-xl font-bold text-slate-900">{brand}</h2>
-              {company && <span className="text-sm text-slate-600">{company}</span>}
-            </div>
-            <Chips chips={tagsCollection.items} />
-          </div>
-          <hr className="pb-10"/>
-          <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-[7fr_5fr] gap-4">
-            <ContentfulRichText document={description?.json} />
-            <div className="px-3 mb-4">
-              { screenshotMobile &&
-                <MobilePreview 
-                    alt={screenshotMobile.description}
-                    url={screenshotMobile.url}
-                  />
-              }
-            </div>
-          </div>
-          <BackButton href='/projects' label='Back to Projects' className='flex md:hidden my-4' />
-        </article>
-    </Layout>
-  );
+export function Project({
+    brand,
+    company,
+    description,
+    screenshotMobile,
+    title,
+    tagsCollection,
+}: IProjectProps) {
+    return (
+        <Layout>
+            <article>
+                <div className="md:flex align-middle pt-10">
+                    <h1 className="text-3xl sm:text-4xl font-extrabold text-slate-800 tracking-tight">
+                        {title}
+                    </h1>
+                    <BackButton
+                        href="/projects"
+                        label="Back to Projects"
+                        className="hidden md:flex ml-auto mt-4 md:mt-0"
+                    />
+                </div>
+                <div className="md:flex py-10">
+                    <div className="grow">
+                        <h2 className="text-xl sm:text-xl font-bold text-slate-900">
+                            {brand}
+                        </h2>
+                        {company && (
+                            <span className="text-sm text-slate-600">
+                                {company}
+                            </span>
+                        )}
+                    </div>
+                    <Chips chips={tagsCollection.items} />
+                </div>
+                <hr className="pb-10" />
+                <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-[7fr_5fr] gap-4">
+                    <ContentfulRichText document={description?.json} />
+                    <div className="px-3 mb-4">
+                        {screenshotMobile && (
+                            <MobilePreview
+                                alt={screenshotMobile.description}
+                                url={screenshotMobile.url}
+                            />
+                        )}
+                    </div>
+                </div>
+                <BackButton
+                    href="/projects"
+                    label="Back to Projects"
+                    className="flex md:hidden my-4"
+                />
+            </article>
+        </Layout>
+    );
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { slug } = context.params as IParams;
-  const { data } = await client.query({
-    query: gql`
-        query($slug: String!) {
-            projectCollection(where: { slug: $slug }) {
+export const getStaticProps: GetStaticProps = async context => {
+    const { slug } = context.params as IParams;
+    const query = gql`
+        query ($preview: Boolean!, $slug: String!) {
+            projectCollection(where: { slug: $slug }, preview: $preview) {
                 items {
-                  brand
-                  company
-                  description {
-                    json
-                  }
-                  screenshotMobile {
-                    description
-                    url
-                  }
-                  slug
-                  title
-                  tagsCollection(limit: 5) {
-                    items {
-                      id
-                      name
-                      icon {
+                    brand
+                    company
+                    description {
+                        json
+                    }
+                    screenshotMobile {
                         description
                         url
-                      }
                     }
-                  }
-                  url
+                    slug
+                    title
+                    tagsCollection(limit: 5) {
+                        items {
+                            id
+                            name
+                            icon {
+                                description
+                                url
+                            }
+                        }
+                    }
+                    url
                 }
             }
         }
-    `,
-    variables: {
-      slug: slug
-    }});
+    `;
+    const contentfulPreview = Boolean(process.env.CONTENTFUL_PREVIEW);
+    const { data } = await client.query({
+        query: query,
+        variables: {
+            slug: slug,
+            preview: contentfulPreview,
+        },
+    });
     const items = data.projectCollection.items[0];
 
-  return {
-    props: {
-      ...items
-    }
-  }
-}
+    return {
+        props: {
+            ...items,
+        },
+    };
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { data } = await client.query({
-    query: gql`
-        query {
-            projectCollection(limit: 10) {
+    const query = gql`
+        query ($preview: Boolean!) {
+            projectCollection(limit: 10, preview: $preview) {
                 items {
                     slug
                 }
             }
         }
-    `});
-  const paths = data.projectCollection.items
-    .map(item => ({ params: { slug: item.slug }}));
+    `;
+    const contentfulPreview = Boolean(process.env.CONTENTFUL_PREVIEW);
+    const { data } = await client.query({
+        query: query,
+        variables: {
+            preview: contentfulPreview,
+        },
+    });
+    const paths = data.projectCollection.items.map(item => ({
+        params: { slug: item.slug },
+    }));
 
-  return {
-    paths,
-    fallback: false
-  }
-}
+    return {
+        paths,
+        fallback: false,
+    };
+};
 
 export default Project;
--- apps/swinburne-dev/pages/projects/index.tsx
+++ apps/swinburne-dev/pages/projects/index.tsx
@@ -12,58 +12,65 @@ export function Projects({ projects }: ProjectsProps) {
     return (
         <Layout>
             <>
-              <div className="grid-cols-1">
-                  <h1 className="text-3xl sm:text-4xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 pt-10">
-                      Projects
-                  </h1>
-              </div>
-              <div className="grid-cols-1 max-w-4xl">
-                  <p className="py-4">
-                      Some key projects that I have been lucky enough to
-                      work on.
-                  </p>
-              </div>
-              <div className="grid lg:grid-cols-3 md:grid-cols-1 gap-4 py-8">
-                  {projects &&
-                      projects.map(project => {
-                          const { title, slug, brand, thumbnail: { description = '', url = ''} } = project;
-                          return (
-                              <ProjectCard
-                                  key={slug}
-                                  title={title}
-                                  excerpt={brand}
-                                  url={`projects/${slug}`}
-                                  imageAlt={description}
-                                  imageUrl={url}
-                              />
-                          );
-                      })}
-              </div>
+                <div className="grid-cols-1">
+                    <h1 className="text-3xl sm:text-4xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 pt-10">
+                        Projects
+                    </h1>
+                </div>
+                <div className="grid-cols-1 max-w-4xl">
+                    <p className="py-4">
+                        Some key projects that I have been lucky enough to work
+                        on.
+                    </p>
+                </div>
+                <div className="grid lg:grid-cols-3 md:grid-cols-1 gap-4 py-8">
+                    {projects &&
+                        projects.map(project => {
+                            const {
+                                title,
+                                slug,
+                                brand,
+                                thumbnail: { description = '', url = '' },
+                            } = project;
+                            return (
+                                <ProjectCard
+                                    key={slug}
+                                    title={title}
+                                    excerpt={brand}
+                                    url={`projects/${slug}`}
+                                    imageAlt={description}
+                                    imageUrl={url}
+                                />
+                            );
+                        })}
+                </div>
             </>
         </Layout>
     );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    const { data } = await client.query({
-        query: gql`
-            query {
-                projectCollection(limit: 10) {
-                    items {
-                        title
-                        brand
-                        description {
-                            json
-                        }
-                        slug
-                        thumbnail {
-                          url
-                          description
-                        }
+    const query = gql`
+        query ($preview: Boolean!) {
+            projectCollection(limit: 10, preview: $preview) {
+                items {
+                    title
+                    brand
+                    slug
+                    thumbnail {
+                        url
+                        description
                     }
                 }
             }
-        `,
+        }
+    `;
+    const contentfulPreview = Boolean(process.env.CONTENTFUL_PREVIEW);
+    const { data } = await client.query({
+        query: query,
+        variables: {
+            preview: contentfulPreview,
+        },
     });
 
     return {
