ea2d33a29b6edd6c1a64eb3e724be9ad4fb35069
Andy Swinburne <andy@swinburne.dev>
2022-12-08 12:35:32 +0000

    add layout tests

--- apps/swinburne-dev/components/layout/layout.spec.tsx
+++ apps/swinburne-dev/components/layout/layout.spec.tsx
@@ -1,10 +1,25 @@
 import { render } from '@testing-library/react';
+import { Layout } from './layout';
 
-import Layout from './layout';
+jest.mock('next/head', () => {
+  return {
+    __esModule: true,
+    default: ({ children }: { children: Array<React.ReactElement> }) => {
+      return <>{children}</>;
+    },
+  };
+});
 
 describe('Layout', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Layout />);
+    const { baseElement } = render(<Layout title='Home'><div>child element</div></Layout>);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render meta title', () => {
+    render(<Layout title='Home'><div>child element</div></Layout>, {
+      container: document.head,
+    });
+    expect(document.title).toBe('Home | Swinburne Dev');
   });
 });
