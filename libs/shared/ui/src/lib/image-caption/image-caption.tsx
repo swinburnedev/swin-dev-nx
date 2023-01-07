1af0186385bdb74e87894d8c48420f603dae4cd7
Andy Swinburne <andy@swinburne.dev>
2023-01-07 18:34:25 +0000

    fix: ts ignore fetchpriority

--- libs/shared/ui/src/lib/image-caption/image-caption.tsx
+++ libs/shared/ui/src/lib/image-caption/image-caption.tsx
@@ -14,6 +14,7 @@ export function ImageCaption({ alt, caption, loading = 'eager', maxWidth = 'full
         className={`max-w-${maxWidth} h-auto rounded-lg`}
         src={url}
         alt={alt}
+        /* @ts-ignore */
         fetchpriority={priority}
         loading={loading}
       />
