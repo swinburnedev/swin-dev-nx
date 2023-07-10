b51dc6741ba3b1723b07e32ab131877bf5b3b5c9
Andy Swinburne <andy@swinburne.dev>
2023-07-10 20:18:05 +0100

    chore(nx): migrate nx devkit

--- tools/generators/new-project/index.ts
+++ tools/generators/new-project/index.ts
@@ -1,20 +1,21 @@
-import { Tree, formatFiles, generateFiles, joinPathFragments, names } from '@nrwl/devkit';
+import {
+  Tree,
+  formatFiles,
+  generateFiles,
+  joinPathFragments,
+  names,
+} from '@nx/devkit';
 
 interface ProjectSchemaOptions {
   title: string;
-  excerpt?: string
+  excerpt?: string;
 }
 export default async function (tree: Tree, schema: ProjectSchemaOptions) {
-  generateFiles(
-    tree,
-    joinPathFragments(__dirname, './files'),
-    './_projects',
-    {
-      title: schema.title,
-      excerpt: schema.excerpt || '',
-      normalizedTitle: names(schema.title).fileName,
-      creationDate: new Date().toISOString()
-    }
-  );
+  generateFiles(tree, joinPathFragments(__dirname, './files'), './_projects', {
+    title: schema.title,
+    excerpt: schema.excerpt || '',
+    normalizedTitle: names(schema.title).fileName,
+    creationDate: new Date().toISOString(),
+  });
   await formatFiles(tree);
 }
