9b18a5387d1659f55fdd88c4e1555d466bfae9b4
Andy Swinburne <andy@swinburne.dev>
2023-07-10 12:59:45 +0100

    chore(nx): bump nextjs v13.3.0

--- apps/swinburne-dev/next.config.js
+++ apps/swinburne-dev/next.config.js
@@ -1,15 +1,15 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require('@nrwl/next/plugins/with-nx');
+const { withNx } = require('@nx/next/plugins/with-nx');
 
 /**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   nx: {
     // Set this to true if you would like to to use SVGR
