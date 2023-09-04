aa60857b8f0d9044f9379e3a34483a477bb5c4cb
Andy Swinburne <andy@swinburne.dev>
2023-09-04 12:06:05 +0100

    add footer

--- apps/test-app/src/app/app.tsx
+++ apps/test-app/src/app/app.tsx
@@ -1,14 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
+import {Footer} from "@swin-dev-nx/shared/ui"
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="test-app" />
-    </div>
-  );
+      <div>
+          <NxWelcome title="test-app" />
+          <Footer />
+      </div>
+  )
 }
 
 export default App;
