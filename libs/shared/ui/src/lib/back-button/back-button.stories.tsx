73e9dfd4e3f45f2816789df0510e807f4af0072e
Andy Swinburne <andy@swinburne.dev>
2023-09-22 11:43:24 +0100

    feat(shared-ui): back button stories

---
+++ libs/shared/ui/src/lib/back-button/back-button.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { BackButton } from './back-button';
+
+const Story: Meta<typeof BackButton> = {
+  component: BackButton,
+  title: 'BackButton',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
