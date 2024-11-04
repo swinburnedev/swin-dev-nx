c62ff9e2002e619d74a4691aabbb9bc0c3ab80f7
Andy Swinburne <andy@swinburne.dev>
2024-11-04 14:49:04 +0000

    chore(root): nx migrate latest

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,22 @@
 module.exports = {
-    stories: ["../**/*.stories.@(js|jsx|ts|tsx|mdx)"],
-    addons: ["@storybook/addon-essentials", "@nx/react/plugins/storybook"],
+    stories: ["../**/*.@(mdx|stories.@(js|jsx|ts|tsx))"],
+    addons: [
+        "@storybook/addon-essentials",
+        "@nx/react/plugins/storybook",
+        "@chromatic-com/storybook",
+        "@storybook/addon-mdx-gfm"
+    ],
+
     framework: {
-        name: "@storybook/react-webpack5",
+        name: "@storybook/nextjs",
         options: {},
     },
+
     docs: {
         autodocs: true,
     },
+
+    typescript: {
+        reactDocgen: "react-docgen-typescript"
+    }
 }
