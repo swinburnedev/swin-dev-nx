06b20fa41640fe523ddbdcc11bfa8078bd91f2d0
Andy Swinburne <andy@swinburne.dev>
2024-11-12 19:26:30 +0000

    fix(img-caption): fetch priority

--- libs/shared/ui/src/lib/image-caption/image-caption.tsx
+++ libs/shared/ui/src/lib/image-caption/image-caption.tsx
@@ -22,7 +22,7 @@ export function ImageCaption({
                 src={url}
                 alt={alt || ""}
                 /* @ts-ignore */
-                fetchpriority={priority}
+                fetchPriority={priority}
                 loading={loading}
             />
             {caption && (
